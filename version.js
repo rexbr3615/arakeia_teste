f688be132eedbbc14c631e9e40dd11f1d1870074
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2024-05-01 00:48:08 -0300

    gregrer

---
+++ version.js
@@ -0,0 +1,3 @@
+$("#Version_salmar").text("0.1a");
+$("#rexbr_salmar_text").text("Rexbr's Host Version -> ");
+$("#copyright").text("Copyright © 2023. All rights reserved. This material may not be reproduced, displayed, modified, or distributed without the express prior written permission of the copyright holder.");
